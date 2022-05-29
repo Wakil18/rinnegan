23aa37c471e427d3c25cd0f5c1eb31210abc69c8
Wakil18 <wakilahmed18@gmail.com>
2022-05-29 09:22:37 +0600

    Core Folder Structure

---
+++ app/libraries/Core.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    *App Core Class
+    *Creates URL & loads core controller
+    *URL format will be - /controller/method/params
+    */
+    class Core{
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = [];
+
+        
+    }
