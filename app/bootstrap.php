00f2aeb1a791d4f6dcfc8c34960752eafe19af62
Wakil18 <wakilahmed18@gmail.com>
2022-05-29 17:03:58 +0600

    AutoLoad Libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,15 @@
     require_once('config/config.php');
 
     //Loading the Libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+    // require_once 'libraries/core.php';
+    // require_once 'libraries/controller.php';
+    // require_once 'libraries/database.php';
+
+
+    // Autoload Core Libraries
+    spl_autoload_register(function($className){
+    /*
+    *In order for this to work, the FILENAME needs to MATCH the CLASS Name
+    */
+        require_once 'libraries/' . $className . '.php';
+    });
