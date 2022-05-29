aca921b508b650a4bacc267b4d58fa1b3fa2b8cc
Wakil18 <wakilahmed18@gmail.com>
2022-05-29 15:36:44 +0600

    Core Class

---
+++ app/controllers/Pages.php
@@ -0,0 +1,12 @@
+<?php
+class Pages{
+    public function __construct(){
+        // echo 'Pages Loaded';
+    }
+
+    public function index(){}
+
+    public function about($id){
+        echo $id;
+    }
+}
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -9,5 +9,53 @@
         protected $currentMethod = 'index';
         protected $params = [];
 
-        
+        public function __construct(){
+            // print_r($this->getUrl());
+            $url = $this->getUrl();
+
+            // Look in the controller for the 1st value
+            if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+
+                // If exixts, set as controller
+                $this->currentController = ucwords($url[0]);
+
+                // Unset 0 index
+                unset($url[0]);
+
+            }
+
+            // Requiring the Controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            // Instentiating the Controller class
+            $this->currentController = new $this->currentController;
+
+            // Check for the method which is the 2nd part or the url
+            if(isset($url[1])){
+                // Check to see if the method exists
+                if(method_exists($this->currentController, $url[1])){
+                    $this->currentMethod = $url[1];
+
+                    // Unset 1 index
+                    unset($url[1]);
+                }
+            }
+
+
+            // Getting the Params
+            $this->params = $url ? array_values($url) : [];
+
+            // Call a callback with the array of params
+            call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+
+        }
+
+        public function getUrl(){
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
+        }
     }
