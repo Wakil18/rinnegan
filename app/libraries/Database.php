4f8d3ba9cf3acb01b36349639a7beaad5fc4a154
Wakil18 <wakilahmed18@gmail.com>
2022-06-01 09:27:17 +0600

    Connected M V C

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -24,7 +24,8 @@ class Database {
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         $options = array(
             PDO::ATTR_PERSISTENT => true, // This will check if there is an extablished connection
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
         );
 
         // Create PDO instance
@@ -34,7 +35,55 @@ class Database {
             $this->error = $e->getMessage();
             echo $this->error;
         }
+    }
 
+    // Prepare statement with query
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+    
+    // Bind Values
+    public function bind($params, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($params, $value, $type);
+    }
+
+    // Execute the prepared statement
+    public function execute(){
+        return $this->stmt->execute();
+    }
+
+    // Get result set as an array of objects
+    public function resultSet(){
+        $this->execute();
+        return $this->stmt->fetchAll();
+    }
+
+    // Get single result as an object
+    public function SingleResult(){
+        $this->execute();
+        return $this->stmt->fetch();
+    }
+
+    // Get row count
+    public function rowCount(){
+        $this->execute();
+        return $this->stmt->rowCount();
     }
 
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -3,3 +3,8 @@ require APPROOT . '/views/includes/header.php';
 require APPROOT . '/views/includes/footer.php';
 ?>
 <h1><?php echo $data['title']; ?></h1>
+<ul>
+    <?php foreach ($data['posts'] as $post) :?>
+        <li><?php echo $post->title ?></li>
+    <?php endforeach; ?>
+</ul>
